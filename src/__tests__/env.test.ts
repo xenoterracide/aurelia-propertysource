aa8e939a5681163d35df0f1dad0684c5290994fd
Caleb Cushing <caleb@7pi.co>
2020-04-10 20:15:59 -0500

    commit more see what happens
    Signed-off-by: Caleb Cushing <caleb@7pi.co>

--- src/__tests__/env.test.ts
+++ src/__tests__/env.test.ts
@@ -1,65 +1,138 @@
-import { MapPropertySource, ObjectPropertySource, ProcessEnvironmentPropertySource } from '../env';
+import { PropertySource } from '../../dist/env';
+import { PropertySourceResolver } from '../EnvironmentResolver';
+import {
+    ImmutablePropertySources,
+    MapPropertySource,
+    ObjectPropertySource,
+    ProcessEnvironmentPropertySource,
+    PropertySourcesPropertyResolver,
+} from '../env';
+import { LogLevel } from '@aurelia/kernel';
 
-describe(`${ ObjectPropertySource.name }`, () => {
-    test("simple", () => {
-        const ps = new ObjectPropertySource("test", { hello: "world" });
+describe(`${PropertySourcesPropertyResolver}`, () => {
+    const envS = new ProcessEnvironmentPropertySource('env', {
+        LOG_LEVEL: 'trace',
+    });
+    const altS = new ObjectPropertySource('prod.config', {
+        log: {
+            dest: 'file',
+            file: 'my.log',
+        },
+    });
+    const objS = new ObjectPropertySource('config', {
+        log: {
+            level: 'debug',
+        },
+    });
+    // defaults
+    const mapS = new MapPropertySource(
+        'defaults',
+        new Map([
+            ['log.level', 'info'],
+            ['log.dest', 'console'],
+            ['log.format', 'theirformat'],
+        ])
+    );
 
-        expect(ps.has("hello")).toBe(true);
-        expect(ps.get("hello")).toBe("world");
-        expect(ps.has("goodbye")).toBe(false);
-        expect(ps.get("goodbye")).toBe(undefined);
+    test(`prod order`, () => {
+        const resolver = new PropertySourcesPropertyResolver(new ImmutablePropertySources(envS, altS, objS, mapS));
+
+        expect(resolver.has('log.level')).toBe(true);
+        expect(resolver.has('log.dest')).toBe(true);
+        expect(resolver.has('log.format')).toBe(true);
+        expect(resolver.has('log.file')).toBe(true);
+
+        expect(resolver.has('log.level')).toBe('trace');
+        expect(resolver.has('log.dest')).toBe('file');
+        expect(resolver.has('log.format')).toBe('theirformat');
+        expect(resolver.has('log.file')).toBe('my.log');
+    });
+});
+
+describe(`${ImmutablePropertySources.name}`, () => {
+    test('unique names same source', () => {
+        new ImmutablePropertySources(new MapPropertySource('a', new Map()), new MapPropertySource('b', new Map()));
+    });
+    test('same names same source', () => {
+        expect(() => {
+            new ImmutablePropertySources(new MapPropertySource('a', new Map()), new MapPropertySource('a', new Map()));
+        }).toThrowError();
+    });
+    test('is array', () => {
+        expect(new ImmutablePropertySources(new ObjectPropertySource('a', {}))).toBeInstanceOf(Array);
+    });
+});
+describe(`${ObjectPropertySource.name}`, () => {
+    test('simple', () => {
+        const ps = new ObjectPropertySource('test', { hello: 'world' });
+
+        expect(ps.has('hello')).toBe(true);
+        expect(ps.get('hello')).toBe('world');
+        expect(ps.has('goodbye')).toBe(false);
+        expect(ps.get('goodbye')).toBe(undefined);
     });
 
-    test("nested", () => {
-        const ps = new ObjectPropertySource("test", {
+    test('nested', () => {
+        const ps = new ObjectPropertySource('test', {
             nested: {
-                key: "val",
+                key: 'val',
             },
         });
 
-        expect(ps.has("nested.key")).toBe(true);
-        expect(ps.get("nested.key")).toBe("val");
-        expect(ps.has("even.more.nested")).toBe(false);
-        expect(ps.get("even.more.nested")).toBe(undefined);
+        expect(ps.has('nested.key')).toBe(true);
+        expect(ps.get('nested.key')).toBe('val');
+        expect(ps.has('even.more.nested')).toBe(false);
+        expect(ps.get('even.more.nested')).toBe(undefined);
     });
 });
-describe(`${ ProcessEnvironmentPropertySource.name }`, () => {
-    test("simple", () => {
-        const ps = new ProcessEnvironmentPropertySource("test", { HELLO: "world" });
+describe(`${ProcessEnvironmentPropertySource.name}`, () => {
+    test('simple', () => {
+        const ps = new ProcessEnvironmentPropertySource('test', { HELLO: 'world' });
 
-        expect(ps.has("hello")).toBe(true);
-        expect(ps.get("hello")).toBe("world");
-        expect(ps.has("goodbye")).toBe(false);
-        expect(ps.get("goodbye")).toBe(undefined);
+        expect(ps.has('hello')).toBe(true);
+        expect(ps.get('hello')).toBe('world');
+        expect(ps.has('goodbye')).toBe(false);
+        expect(ps.get('goodbye')).toBe(undefined);
     });
 
-    test("nested", () => {
-        const ps = new ProcessEnvironmentPropertySource("test", {
-            NESTED_KEY: "val",
+    test('nested', () => {
+        const ps = new ProcessEnvironmentPropertySource('test', {
+            NESTED_KEY: 'val',
+        });
+        expect(ps.has('nested.key')).toBe(true);
+        expect(ps.get('nested.key')).toBe('val');
+        expect(ps.has('even.more.nested')).toBe(false);
+        expect(ps.get('even.more.nested')).toBe(undefined);
+    });
+
+    test('nested', () => {
+        const ps = new ProcessEnvironmentPropertySource('test', {
+            A: 'B',
+            A_B: 'C',
+            A_B_C: 'D',
         });
 
-        expect(ps.has("nested.key")).toBe(true);
-        expect(ps.get("nested.key")).toBe("val");
-        expect(ps.has("even.more.nested")).toBe(false);
-        expect(ps.get("even.more.nested")).toBe(undefined);
+        expect(ps.get('a')).toBe('B');
+        expect(ps.get('a.b')).toBe('C');
+        expect(ps.get('a.b.c')).toBe('D');
     });
 });
-describe(`${ MapPropertySource.name }`, () => {
-    test("simple", () => {
-        const ps = new MapPropertySource("test", new Map([ [ "hello", "world" ] ]));
+describe(`${MapPropertySource.name}`, () => {
+    test('simple', () => {
+        const ps = new MapPropertySource('test', new Map([['hello', 'world']]));
 
-        expect(ps.has("hello")).toBe(true);
-        expect(ps.get("hello")).toBe("world");
-        expect(ps.has("goodbye")).toBe(false);
-        expect(ps.get("goodbye")).toBe(undefined);
+        expect(ps.has('hello')).toBe(true);
+        expect(ps.get('hello')).toBe('world');
+        expect(ps.has('goodbye')).toBe(false);
+        expect(ps.get('goodbye')).toBe(undefined);
     });
 
-    test("nested", () => {
-        const ps = new MapPropertySource("test", new Map([ [ "nested.key", "val" ] ]));
+    test('nested', () => {
+        const ps = new MapPropertySource('test', new Map([['nested.key', 'val']]));
 
-        expect(ps.has("nested.key")).toBe(true);
-        expect(ps.get("nested.key")).toBe("val");
-        expect(ps.has("even.more.nested")).toBe(false);
-        expect(ps.get("even.more.nested")).toBe(undefined);
+        expect(ps.has('nested.key')).toBe(true);
+        expect(ps.get('nested.key')).toBe('val');
+        expect(ps.has('even.more.nested')).toBe(false);
+        expect(ps.get('even.more.nested')).toBe(undefined);
     });
 });
--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,24 @@
 import { constantCase } from 'constant-case';
 import { JSONPath } from 'jsonpath-plus';
+import { value } from './decorator';
+import { DI, optional } from '@aurelia/kernel';
+
+export const RequiredProperties = DI.createInterface<Array<string>>('RequiredProperties').noDefault();
 
 interface Getter<K, T> {
     has(key: K): boolean;
     get(key: string): T;
+}
+
+export interface PropertyResolver {
+    has(key: string): boolean;
+    get(key: string): unknown | undefined;
+    get<T>(key: string): T | undefined;
+    get<T>(key: string, defaultvalue: T): T;
+
+    getRequired(key: string): unknown;
+    getRequired<T>(key: string): T;
+    getRequired<T>(key: string, defaultvalue: T): T;
 }
 
 export interface PropertySource<T = unknown> extends Getter<string, unknown | undefined> {
@@ -13,6 +28,53 @@ export interface PropertySource<T = unknown> extends Getter<string, unknown | un
 
 export interface EnumerablePropertySource<T> extends PropertySource<T> {
     keys(): Iterable<string>;
+}
+
+export interface KVSource<T = boolean | number | string | KVSource<unknown>> {
+    [key: string]: T | undefined;
+}
+
+export interface PropertySources extends Getter<string, PropertySource>, Array<PropertySource> {}
+
+export class PropertySourcesPropertyResolver implements PropertyResolver {
+    readonly #propertySources: PropertySources;
+    readonly #requiredProperites: string[];
+    constructor(propertySources: PropertySources, @optional(RequiredProperties) requiredProperties?: string[]) {
+        this.#propertySources = propertySources;
+        this.#requiredProperites = requiredProperties ?? [];
+    }
+    has(key: string): boolean {
+        return this.#propertySources.some((ps) => ps.has(key));
+    }
+    get<T = unknown>(key: string, defaultvalue?: T): T | unknown | undefined {
+        return this.#propertySources.find((ps) => ps.has(key))?.get(key) ?? defaultvalue;
+    }
+    getRequired<T>(key: any, defaultvalue?: any): T | unknown | undefined {
+        const val = this.get(key, defaultvalue);
+        if (val === undefined) {
+            throw new Error(`value for ${key} not found`);
+        }
+        return val;
+    }
+}
+
+export class ImmutablePropertySources extends Array<PropertySource> implements PropertySources {
+    readonly #map: Map<string, PropertySource>;
+    constructor(ps: PropertySource, ...propertySources: PropertySource[]) {
+        super();
+        this.push(ps, ...propertySources);
+        this.#map = new Map(propertySources.map((ps) => [ps.name, ps]));
+        if (this.length !== this.#map.size) {
+            throw new Error(`you should not have duplicate named property sources ${this.map((p) => p.name)}`);
+        }
+    }
+
+    has(key: string): boolean {
+        return this.#map.has(key);
+    }
+    get(key: string): PropertySource<unknown> {
+        return this.get(key);
+    }
 }
 
 export class MapPropertySource<T = unknown> implements EnumerablePropertySource<Map<string, T>> {
@@ -28,32 +90,6 @@ export class MapPropertySource<T = unknown> implements EnumerablePropertySource<
 
     get(key: string): T {
         return this.source.get(key) as T;
-    }
-}
-
-export interface KVSource<T = boolean | number | string | KVSource<unknown>> {
-    [key: string]: T | undefined;
-}
-
-export interface PropertySources extends Getter<string, PropertySource>, Iterable<PropertySource> {}
-
-export class ImmutablePropertySources implements PropertySources {
-    readonly #map: Map<string, PropertySource>;
-    readonly #list: Array<PropertySource>;
-
-    constructor(propertySources: PropertySource[]) {
-        this.#map = new Map(propertySources.map((ps) => [ps.name, ps]));
-        this.#list = propertySources;
-    }
-    [Symbol.iterator](): Iterator<PropertySource<unknown>, unknown, undefined> {
-        return this.#list[Symbol.iterator]();
-    }
-
-    has(key: string): boolean {
-        return this.#map.has(key);
-    }
-    get(key: string): PropertySource<unknown> {
-        return this.get(key);
     }
 }
 

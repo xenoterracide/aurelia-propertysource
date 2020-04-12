f9c96f1f0c075dca2a9dc3112ab6ec305590e270
Caleb Cushing <caleb@7pi.co>
2020-04-12 18:37:15 -0500

    change key name to be easier reading
    Signed-off-by: Caleb Cushing <caleb@7pi.co>

--- src/di.ts
+++ src/di.ts
@@ -3,20 +3,6 @@ import '@aurelia/metadata';
 import { Constructable, IContainer, IRegistration, IResolver, Resolved } from '@aurelia/kernel';
 import { Environment } from './env';
 
-export function valueDecorator(property: string): Function {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    return function (ctor: Constructable, _: undefined, position: number): void {
-        const data: [string, number] = [property, position];
-        Reflect.defineMetadata('aupropertysource:value', data, ctor);
-    };
-}
-
-export function value<T extends string>(): typeof valueDecorator;
-export function value<T extends string>(target: T): Function;
-export function value<T extends string>(target?: T): Function | typeof valueDecorator {
-    return target == null || target == undefined ? valueDecorator : valueDecorator(target);
-}
-
 export class EnvironmentResolver implements IResolver, IRegistration {
     constructor(private readonly key: string) {}
 
@@ -30,4 +16,17 @@ export class EnvironmentResolver implements IResolver, IRegistration {
 
 export function property(key: string): IResolver {
     return new EnvironmentResolver(key);
+}
+
+export function valueDecorator(property: string): Function {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return function (ctor: Constructable, _: undefined, position: number): IResolver {
+        const data: [string, number] = [property, position];
+        return new EnvironmentResolver(property);
+    };
+}
+export function value<T extends string>(): typeof valueDecorator;
+export function value<T extends string>(target: T): Function;
+export function value<T extends string>(target?: T): Function | typeof valueDecorator {
+    return target == null || target == undefined ? valueDecorator : valueDecorator(target);
 }
--- test/di.spec.ts
+++ test/di.spec.ts
@@ -1,18 +1,30 @@
-import { DI, Registration } from '@aurelia/kernel';
-import { property } from '../src/di';
+import { DI, IContainer, Registration } from '@aurelia/kernel';
+import { property, value } from '../src/di';
 import { Environment, ImmutablePropertySources, ObjectPropertySource, StandardEnvironment } from '../src/env';
 
 describe(`Aurelia DI test`, () => {
+    let container: IContainer;
     const myDefault = new ObjectPropertySource('default', {
-        a: 1,
+        myprop: 1,
     });
 
-    test(`test`, () => {
-        const container = DI.createContainer();
+    beforeEach(() => {
+        container = DI.createContainer();
         container.register(
             Registration.instance(Environment, new StandardEnvironment(new ImmutablePropertySources(myDefault)))
         );
+    });
+
+    test('direct get', () => {
         container.register(property('a'));
         expect(container.get('a')).toBe(1);
+    });
+
+    test('get injected', () => {
+        class Foo {
+            constructor(@value('myprop') readonly myprop: string) {}
+        }
+
+        expect(container.get(Foo).myprop).toBe(1);
     });
 });
